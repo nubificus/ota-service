dee20f8d2e4c0a3fc21ae19d3de86a60a3c48ed8
Ilias Lagomatis <ilago@nubificus.co.uk>
2025-02-24 15:46:12 +0200

    feat: Secure boot V2 attempt
    Signed-off-by: Ilias Lagomatis <ilago@nubificus.co.uk>

--- src/dice_cert.c
+++ src/dice_cert.c
@@ -31,15 +31,150 @@
 
 #include "dice_cert.h"
 
-#define BOOTLOADER_HASH_OFFSET 0x80
-#define BOOTLOADER_HASH_SIZE   64
+#include "esp_efuse.h"
+#include "esp_efuse_table.h"
+#include "soc/efuse_reg.h"
 
-#define APP_HASH_OFFSET 0x200b0
-#define APP_HASH_SIZE   16
+#include "esp_ota_ops.h"
+#include "esp_image_format.h"
+#include "esp_crc.h"
+#include "spi_flash_mmap.h"
 
 #define DEBUG 1
 
+#define MAGIC 0xe7
+#define V2 0x02
+#define JUMP (64 * 1024)
+#define HASH_OFF 4
+#define HASH_SIZE 32
+#define CSR_OFF 1196
+#define PAD_OFF 1200
+#define PAD_LEN 16
+#define PAD "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+
+#define BOOTLOADER_HASH_SIZE 64
+#define APP_HASH_SIZE   16
+
 static const char *TAG = "dice-cert-gen";
+
+static int dice_config_v2(DiceInputValues *input_values) {
+	uint32_t flash_size;
+	esp_err_t err = esp_flash_get_size(NULL, &flash_size);
+
+	if (err == ESP_OK) {
+		ESP_LOGI(TAG, "Flash size: %d bytes (%.2f MB)",
+			(int) flash_size, (float) flash_size / (1024.0 * 1024.0));
+	} else {
+		ESP_LOGE(TAG, "Failed to get flash size: %s", esp_err_to_name(err));
+		return -1;
+	}
+
+	bool boot_hash_written = false;
+	const uint32_t jump = JUMP;
+	for (uint32_t offset = jump; offset < flash_size; offset += jump) {
+		const void *mm = NULL;
+		const size_t size = 4096;
+		spi_flash_mmap_handle_t mm_handle = 0;
+
+		esp_err_t err = spi_flash_mmap(offset, size, SPI_FLASH_MMAP_DATA,
+					       &mm, &mm_handle);
+		if (err != ESP_OK) {
+			printf("Failed to map flash region: %d\n", err);
+			return -1;
+		}
+
+                const uint8_t *bytes = (const uint8_t *) mm;
+
+                /* First condition */
+                if (bytes[0] != MAGIC && bytes[1] != V2 && bytes[2] && bytes[3])
+                        goto unmap;
+
+                /* Second condition */
+                if (*((uint32_t *)(bytes + CSR_OFF)) != esp_crc32_le(0, bytes, CSR_OFF))
+                        goto unmap;
+
+                /* Third condition */
+                if (memcmp(bytes + PAD_OFF, PAD, PAD_LEN))
+                        goto unmap;
+
+                printf("Found signature block at offset: %d\n", (int) offset);
+
+		if (!boot_hash_written) {
+			printf("Writing boot hash..\n");
+			memcpy(input_values->code_hash, &bytes[HASH_OFF], HASH_SIZE);
+			memcpy(input_values->code_hash + HASH_SIZE, &bytes[HASH_OFF],
+			       HASH_SIZE);
+			boot_hash_written = true;
+		} else {
+			printf("Writing application hash..\n");
+			memset(input_values->config_value, 0, sizeof(input_values->config_value));
+			memcpy(input_values->config_value, &bytes[HASH_OFF], APP_HASH_SIZE);
+			spi_flash_munmap(mm_handle);
+			return 0;
+		}
+unmap:
+		spi_flash_munmap(mm_handle);
+	}
+	return -1;
+}
+
+typedef enum {
+	SECURE_BOOT_NONE = 0,
+	SECURE_BOOT_V1,
+	SECURE_BOOT_V2
+} secure_boot_version_t;
+
+#define BOOTLOADER_HASH_OFFSET 0x80
+#define APP_HASH_OFFSET 0x200b0
+
+secure_boot_version_t secure_boot_version() {
+#ifdef CONFIG_IDF_TARGET_ESP32
+	if (esp_efuse_read_field_bit(ESP_EFUSE_ABS_DONE_0))
+		return SECURE_BOOT_V1;
+#elif defined(CONFIG_IDF_TARGET_ESP32S2) || \
+	defined(CONFIG_IDF_TARGET_ESP32S3) || \
+	defined(CONFIG_IDF_TARGET_ESP32C3)
+
+	if (esp_efuse_read_field_bit(ESP_EFUSE_SECURE_BOOT_EN))
+		return SECURE_BOOT_V2;
+#endif
+	return SECURE_BOOT_NONE;
+}
+
+static void dice_config_none(DiceInputValues *input_values) {
+	memset(input_values->code_hash, 0, sizeof(input_values->code_hash));
+	memset(input_values->config_value, 0, sizeof(input_values->config_value));
+}
+
+static void dice_config_v1(DiceInputValues *input_values) {
+	esp_flash_read(NULL, input_values->code_hash, BOOTLOADER_HASH_OFFSET,
+		       BOOTLOADER_HASH_SIZE);
+	memset(input_values->config_value, 0, sizeof(input_values->config_value));
+	esp_flash_read(NULL, input_values->config_value, APP_HASH_OFFSET,
+		       APP_HASH_SIZE);
+}
+
+static void dice_config(DiceInputValues *input_values) {
+	secure_boot_version_t version = secure_boot_version();
+	switch (version) {
+	case SECURE_BOOT_V1:
+		printf("Secure Boot: V1\n");
+		dice_config_v1(input_values);
+		break;
+	case SECURE_BOOT_V2:
+		printf("Secure Boot: V2\n");
+		if (dice_config_v2(input_values) == 0)
+			break;
+		printf("Secure Boot V2 DICE Configuarion failed..\n");
+	case SECURE_BOOT_NONE:
+		printf("Configuring DICE configuration without hashes\n");
+		dice_config_none(input_values);
+		break;
+	}
+
+	input_values->mode = kDiceModeNormal;
+	input_values->config_type = kDiceConfigTypeInline;
+}
 
 static const uint8_t asym_salt[] = {
     0x63, 0xB6, 0xA0, 0x4D, 0x2C, 0x07, 0x7F, 0xC1, 0x0F, 0x63, 0x9F,
@@ -50,9 +185,9 @@ static const uint8_t asym_salt[] = {
     0xD1, 0xAF, 0xFB, 0x78, 0x9C, 0xCD, 0x2B, 0x0B, 0x3B};
 
 int gen_dice_cert(void *buf, size_t max_len) {
-	uint8_t final_seal_cdi_buffer[DICE_CDI_SIZE] = {0};
-	uint8_t final_cdi_buffer[DICE_CDI_SIZE] = {0};
-	DiceInputValues input_values = {0};
+	uint8_t final_seal_cdi_buffer[DICE_CDI_SIZE] = { 0 };
+	uint8_t final_cdi_buffer[DICE_CDI_SIZE] = { 0 };
+	DiceInputValues input_values = { 0 };
 	uint8_t cert_buffer[2048] = { 0 };
 	DiceResult dice_ret;
 	int ret, i;
@@ -96,13 +231,7 @@ int gen_dice_cert(void *buf, size_t max_len) {
 		goto fail;
 	}
 
-	input_values.mode = kDiceModeNormal;
-	input_values.config_type = kDiceConfigTypeInline;
-	esp_flash_read(NULL, input_values.code_hash, BOOTLOADER_HASH_OFFSET,
-		       BOOTLOADER_HASH_SIZE);
-	memset(input_values.config_value, 0, sizeof(input_values.config_value));
-	esp_flash_read(NULL, input_values.config_value, APP_HASH_OFFSET,
-		       APP_HASH_SIZE);
+	dice_config(&input_values);
 
 	dice_ret = DiceMainFlow(NULL, uds_buffer, uds_buffer, &input_values,
 				sizeof(cert_buffer), cert_buffer,
